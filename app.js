828634d923ae679fcf2c31a37cf5ceb763740757
Bigless27 <lessonryan@gmail.com>
2016-02-24 13:20:01 -0600

    Combine two controllers into one

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-angular.module('rockPaperScissors').controller('MainController', ['$scope', function($scope){
-    $scope.choices = ['Rock', 'Paper', 'Scissors']
-    $scope.pick = {choice: ''}
+angular.module("rockPaperScissors").controller("MainController",['$scope', function($scope){
+
+  this.main = MainController.pick
+  $scope.choices = ['Rock', 'Paper', 'Scissors']
+  $scope.pick = {choice: ''}
+
+  $scope.results = ""
+  $scope.playGame = function(){
+    this.results = ryan.play(this.main);
+  }
 }]);
