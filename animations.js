773454de04cbe2d28d2b69f169c2536247c3325c
Bigless27 <lessonryan@gmail.com>
2016-03-01 16:45:44 -0600

    Synce play with hand movements

--- animations.js
+++ animations.js
@@ -1,3 +1,14 @@
 angular.module("rockPaperScissors").controller("AnimationsController",['$scope',function($scope){
+    $('.animate').hide()
+    function removeAnimationClass(){
+      $('.animate').removeClass("animation")
+    }
+    $scope.shake = function(){
+      $('.animate').show()
+      $('.animate').addClass("animation")
+      $scope.results
+      setTimeout(removeAnimationClass,3000)
+    }
+
 
 }])
