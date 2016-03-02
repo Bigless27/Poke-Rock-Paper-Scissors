1b687acaa901e2224112294acf5f76eeab776798
Bigless27 <lessonryan@gmail.com>
2016-03-02 13:14:24 -0600

    Put angular call in document.ready

--- animations.js
+++ animations.js
@@ -1,22 +1,24 @@
 angular.module("rockPaperScissors").controller("AnimationsController",['$scope',function($scope){
-    $('.animate').hide()
-    function removeAnimationClass(){
-      $('.animate').removeClass("animation");
-      $('.play-button').removeAttr('disabled');
-      $('.results').show();
-      $('.computer-choice .choice').show();
-      $('.result-picture ').show();
-    }
-    $scope.shake = function(){
-      $('.animate').show();
-      $('.animate').addClass("animation");
-      $('.play-button').attr('disabled', 'disabled');
-      $('.results').hide();
-      $('.computer-choice .choice').hide();
-      $('.result-picture').hide()
-      setTimeout(removeAnimationClass,2000);
+  angular.element(document).ready(function(){
 
-    }
-
-
+      $('.animate').hide()
+      $scope.pokemon = ''
+      function removeAnimationClass(){
+        $('.animate').removeClass("animation");
+        $('.play-button').removeAttr('disabled');
+        $('.results').show();
+        $('.computer-choice .choice').show();
+        $('.result-picture ').show();
+      }
+      $scope.shake = function(){
+        $('.animate').show();
+        $('.animate').addClass("animation");
+        $('.play-button').attr('disabled', 'disabled');
+        $('.results').hide();
+        $('.computer-choice .choice').hide();
+        $('.result-picture').hide()
+        setTimeout(removeAnimationClass,2000)
+      }
+      console.log(pokedex)
+  })
 }])
