f021f4c734a2f8dddb9a26ccea81767fd7553b13
Bigless27 <lessonryan@gmail.com>
2016-03-04 14:15:06 -0600

    Write test for functionality of MainController

---
+++ Spec/mainControllerSpec.js
@@ -0,0 +1,81 @@
+describe("MainController", function(){
+    beforeEach(module('rockPaperScissors'));
+
+    var $controller;
+
+    beforeEach(inject(function(_$controller_){
+        $controller = _$controller_;
+    }))
+
+    describe('$scope.playGame',function(){
+        it("Returns a win a lose or a draw result",function(){
+
+            var $scope = {};
+            var controller = $controller('MainController', {$scope:$scope});
+            $scope.pick.choice = "Fire";
+            $scope.playGame();
+            var matcher;
+            if($scope.results === "You Win"){
+                matcher = "You Win";
+            }
+            else if($scope.results === "Computer Wins"){
+                matcher = "Computer Wins";
+            }
+            else if($scope.results === "Draw"){
+                matcher = "Draw";
+            }
+            expect($scope.results).toEqual(matcher);
+        })
+    })
+
+    describe("scope.playGame", function(){
+        it("Increments user stats by one if it is a win, or if it's a draw does nothing", function(){
+
+            var $scope = {}
+            var controller = $controller('MainController', {$scope: $scope});
+
+            var matcher;
+            var value;
+            $scope.playGame();
+            if($scope.results === "You Win"){
+                matcher = $scope.wins;
+                value = 1;
+
+            }
+            else if($scope.results === "Computer Wins"){
+                matcher = $scope.losses;
+                value = 1;
+            }
+            else if($scope.results === "Draw"){
+                matcher = $scope.wins
+                value = 0;
+            }
+            expect(matcher).toEqual(value);
+        })
+    })
+
+    describe("scope.resetStats", function(){
+        it("Resets user's win stats to zero", function(){
+
+            var $scope = {};
+            var controller = $controller('MainController', {$scope:$scope});
+            $scope.wins = 5;
+            $scope.resetStats();
+
+            expect($scope.wins).toEqual(0);
+
+        })
+    })
+
+    describe("scope.resetStats", function(){
+        it("Resets user's lose stats to zero", function(){
+
+            var $scope = {};
+            var controller = $controller('MainController', {$scope:$scope});
+            $scope.losses = 5;
+            $scope.resetStats();
+
+            expect($scope.losses).toEqual(0);
+        })
+    })
+})
