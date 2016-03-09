744fce3c220689fec8283af4389503d364dcdbb3
Bigless27 <lessonryan@gmail.com>
2016-03-09 17:50:32 -0600

    Hide and show images at right moment

--- animations.js
+++ animations.js
@@ -2,10 +2,36 @@ angular.module("rockPaperScissors").controller("AnimationsController",['$scope',
   angular.element(document).ready(function(){
       $scope.pokemonPlayer = '';
       $scope.pokemonComputer = '';
+      $('.animate').hide();
+      $('.pokepic').hide();
+
       $scope.showPokemon = function(){
         var pokemonController = new pokeController
+        $('.animate').addClass("animation")
         $scope.pokemonPlayer = pokemonController.pokemonPlayerDisplay($scope.pick.choice);
         $scope.pokemonComputer = pokemonController.pokemonComputerDisplay($scope.computerChoice)
+        shakeDisplay();
+      }
+
+      function shakeDisplay(){
+        $(".pokepic").hide();
+        $('.animate').show();
+        $('.animate').addClass("animation");
+        $('.play-button').attr('disabled', 'disabled');
+        $('.results').hide();
+        $('.computer-choice .choice').hide();
+        $('.result-picture').hide()
+        setTimeout(removeAnimationClass,2000)
+      }
+
+      function removeAnimationClass(){
+        $('.animate').removeClass("animation");
+        $('.play-button').removeAttr('disabled');
+        $('.results').show();
+        $('.computer-choice .choice').show();
+        $('.result-picture ').show();
+        $(".pokepic").show();
+
       }
   })
 }])
