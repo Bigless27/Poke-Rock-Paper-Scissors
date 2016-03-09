aa6001264109ada4630ea256cbeabe169260740f
Bigless27 <lessonryan@gmail.com>
2016-03-09 17:50:50 -0600

    Seperate increment function from $scope

--- app.js
+++ app.js
@@ -10,17 +10,7 @@ angular.module("rockPaperScissors").controller("MainController",['$scope', funct
       var gamePlay = new Controller($scope.pick.choice);
       $scope.results = gamePlay.play;
       $scope.computerChoice = gamePlay.computersPick;
-      if ($scope.results === "You Win"){
-        $scope.wins += 1;
-      }
-      else if($scope.results == "Gendelman Wins"){
-        $scope.losses += 1;
-      }
-
-      if ($scope.wins === 10 ){
-        $scope.wins += 1
-        swal("Congrats!!!", "You Defeated the Evil Gendelman!", "success")
-      }
+      incrementResults();
     }
 
     $scope.resetStats = function(){
@@ -28,6 +18,19 @@ angular.module("rockPaperScissors").controller("MainController",['$scope', funct
       $scope.wins = 0;
       $scope.losses = 0;
       $scope.computerChoice = "None";
+    }
+
+    function incrementResults(){
+      if ($scope.results === "You Win"){
+        $scope.wins += 1;
+      }
+      else if($scope.results == "Gendelman Wins"){
+        $scope.losses += 1;
+      }
+      if ($scope.wins === 10 ){
+        $scope.wins += 1
+        swal("Congrats!!!", "You Defeated the Evil Gendelman!", "success")
+      }
     }
 }]);
 
