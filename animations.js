3b47e01bc44223628345a0c9cfd1ccc94f1a788e
Bigless27 <lessonryan@gmail.com>
2016-03-09 20:13:26 -0600

    Refactor results diplay

--- animations.js
+++ animations.js
@@ -14,24 +14,17 @@ angular.module("rockPaperScissors").controller("AnimationsController",['$scope',
       }
 
       function shakeDisplay(){
-        $(".pokepic").hide();
         $('.animate').show();
         $('.animate').addClass("animation");
         $('.play-button').attr('disabled', 'disabled');
-        $('.results').hide();
-        $('.computer-choice .choice').hide();
-        $('.result-picture').hide()
+        $('.display').hide()
         setTimeout(removeAnimationClass,2000)
       }
 
       function removeAnimationClass(){
         $('.animate').removeClass("animation");
         $('.play-button').removeAttr('disabled');
-        $('.results').show();
-        $('.computer-choice .choice').show();
-        $('.result-picture ').show();
-        $(".pokepic").show();
-
+        $('.display').show()
       }
   })
 }])
