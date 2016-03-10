2894212c8269846ce27039ab333c508df6260a41
Bigless27 <lessonryan@gmail.com>
2016-03-09 20:53:10 -0600

    Add initial coloring to radio buttons

---
+++ radio-styling.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('#Grass').parent().parent().addClass('Grass')
+  $('#Water').parent().parent().addClass('Water')
+  $('#Fire').parent().parent().addClass('Fire')
+})
