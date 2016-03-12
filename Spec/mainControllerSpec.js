9d4a27116c79751a4eb6c68c2e74888b037b34e3
Bigless27 <lessonryan@gmail.com>
2016-03-11 21:36:40 -0600

    Add controller object test and refactor method test

--- Spec/mainControllerSpec.js
+++ Spec/mainControllerSpec.js
@@ -8,23 +8,13 @@ describe("MainController", function(){
     }))
 
     describe('$scope.playGame',function(){
-        it("Returns a win a lose or a draw result",function(){
+        it("Returns a win, a lose or a draw result",function(){
 
             var $scope = {};
             var controller = $controller('MainController', {$scope:$scope});
             $scope.pick.choice = "Fire";
             $scope.playGame();
-            var matcher;
-            if($scope.results === "You Win"){
-                matcher = "You Win";
-            }
-            else if($scope.results === "Computer Wins"){
-                matcher = "Computer Wins";
-            }
-            else if($scope.results === "Draw"){
-                matcher = "Draw";
-            }
-            expect($scope.results).toEqual(matcher);
+            expect(["You Win", "Gendelman Wins", "Draw"].includes($scope.results)).toBeTruthy();
         })
     })
 
